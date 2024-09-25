a84b085a2d11ebdf7c2b883ddf3a0592936df91d
Literally A Penguin <crazysnipercatt0@proton.me>
2024-09-25 18:15:21 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rand::Rng;
 
 fn main() {
-    println!("Cock needs boing boing?");
+    println!("How many bees will you get?");
+    println!("Guess your answer");
+    println!("That was wrong, dipshit");
     let num = rand::thread_rng().gen_range(0..100);
-    print!("number");
+    print!("The amount of bee's you get: ");
     println!("{}", num);
 
 }
